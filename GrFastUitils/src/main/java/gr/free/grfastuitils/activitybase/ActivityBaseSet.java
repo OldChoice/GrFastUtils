1c0f8ac2ac896e7ad0f6b043a598ac6043833165
user.name <844378772@qq.com>
2019-11-22 16:25:34 +0800

    baseactivity

---
+++ GrFastUitils/src/main/java/gr/free/grfastuitils/activitybase/ActivityBaseSet.java
@@ -0,0 +1,8 @@
+package gr.free.grfastuitils.activitybase;
+
+public interface ActivityBaseSet {
+	public abstract void findView();
+	public abstract void initValues();
+	public abstract void setView();
+	public abstract void setListener();
+}
---
+++ GrFastUitils/src/main/java/gr/free/grfastuitils/activitybase/AppManager.java
@@ -0,0 +1,107 @@
+package gr.free.grfastuitils.activitybase;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.Stack;
+
+/**
+ * 这是一个Activity管理类，管理Activity，控制程序。达到安全退出。 用了单列模式
+ * 
+ * @author ying
+ * 
+ */
+public class AppManager {
+
+	private static Stack<Activity> activityStack;
+	private static AppManager instance;
+
+	private AppManager() {
+	}
+
+	/**
+	 * 单一实例
+	 */
+	public static AppManager getAppManager() {
+		if (instance == null) {
+			instance = new AppManager();
+		}
+		return instance;
+	}
+
+	/**
+	 * 添加Activity到堆栈中
+	 */
+	public void addActivity(Activity activity) {
+		if (activityStack == null) {
+			activityStack = new Stack<Activity>();
+		}
+		activityStack.add(activity);
+	}
+
+	/**
+	 * 获取当前Activity（堆栈中后入先出）
+	 */
+	public Activity currentActivity() {
+		Activity activity = activityStack.lastElement();
+		return activity;
+	}
+
+	/**
+	 * 结束当前Activity（堆栈中后入先出）
+	 */
+	public void finishActivity() {
+		Activity activity = activityStack.lastElement();
+		finishActivity(activity);
+	}
+
+	/**
+	 * 结束指定的Activity
+	 */
+	public void finishActivity(Activity activity) {
+		if (activity != null) {
+			activityStack.remove(activity);
+			activity.finish();
+			activity = null;
+		}
+	}
+
+	/**
+	 * 结束指定类名的Activity
+	 */
+	public void finishActivity(Class<?> cls) {
+		for (Activity activity : activityStack) {
+			if (activity.getClass().equals(cls)) {
+				finishActivity(activity);
+			}
+		}
+	}
+
+	/**
+	 * 结束所以的Activity
+	 */
+	public void finishAllActivity() {
+		for (int i = 0, size = activityStack.size(); i < size; i++) {
+			if (null != activityStack.get(i)) {
+				activityStack.get(i).finish();
+			}
+		}
+		activityStack.clear();
+	}
+
+	/**
+	 * 彻底退出应用程序，安全退出
+	 */
+	public void AppExit(Context context) {
+		try {
+			finishAllActivity();
+			ActivityManager activityMgr = (ActivityManager) context
+					.getSystemService(Context.ACTIVITY_SERVICE);
+			activityMgr.restartPackage(context.getPackageName());
+			System.exit(0);
+			android.os.Process.killProcess(android.os.Process.myPid());
+		} catch (Exception e) {
+		}
+	}
+}
